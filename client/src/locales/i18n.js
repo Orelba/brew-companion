5fe5339ba524a07c8650899410541e7face95301
Orelba <88292816+Orelba@users.noreply.github.com>
2025-04-29 19:33:27 +0300

    Refactor i18n structure

---
+++ client/src/locales/i18n.js
@@ -0,0 +1,25 @@
+import i18next from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+
+import enTranslation from './en.json'
+import heTranslation from './he.json'
+
+i18next
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    // debug: import.meta.env.MODE === 'development', // Set debug to true only in development
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false, // React already handles escaping
+    },
+    resources: {
+      en: {
+        translation: enTranslation,
+      },
+      he: {
+        translation: heTranslation,
+      },
+    },
+  })
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import '@mantine/core/styles.css' // Mantine core styles
 import '@mantine/notifications/styles.css' // Mantine notifications styles
 import '@mantine/carousel/styles.css' // Mantine carousel styles
 import '@mantine/charts/styles.css' // Mantine charts styles
-import './i18n.js' // i18n initialization
+import './locales/i18n.js' // i18n initialization
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
