a6220a3bba0cdcf049ebb8adec769d5affea6bd2
Orelba <88292816+Orelba@users.noreply.github.com>
2025-05-04 02:50:19 +0300

    Remove redundant imports from brewingMethodsController.js

--- server/controllers/brewingMethodsController.js
+++ server/controllers/brewingMethodsController.js
@@ -1,7 +1,6 @@
 import BrewingMethod from '../models/brewingMethod.js'
 
 import asyncHandler from 'express-async-handler'
-import { body, validationResult } from 'express-validator'
 
 const brewingMethodsList = asyncHandler(async (req, res, next) => {
   const allBrewingMethods = await BrewingMethod.find({}).exec()
