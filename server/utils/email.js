8c0905fd839a8db5da11ef6741fbd4a82c9841af
Orelba <88292816+Orelba@users.noreply.github.com>
2025-05-30 02:54:32 +0300

    server: change email config to be used with Gmail

--- server/utils/email.js
+++ server/utils/email.js
@@ -3,16 +3,15 @@ import nodemailer from 'nodemailer'
 const sendEmail = async (options) => {
   // Create a transporter
   const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    secure: false, // true for 465, false for other ports
+    service: 'gmail',
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
     },
-    tls: {
-      rejectUnauthorized: process.env.NODE_ENV === 'development' ? false : true, // Bypass certificate validation
-    },
+    tls:
+      process.env.NODE_ENV === 'development'
+        ? { rejectUnauthorized: false }
+        : undefined,
   })
 
   const emailOptions = {
