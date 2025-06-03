8de27acd0fb87b295598356ec69aef04679ba4aa
Orelba <88292816+Orelba@users.noreply.github.com>
2025-06-03 06:06:42 +0300

    feat(pwa): set up PWA support

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,59 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'node:path'
+import { VitePWA } from 'vite-plugin-pwa'
+
+const manifestForPlugin = {
+  registerType: 'autoUpdate',
+  includeAssets: [
+    'favicon.svg',
+    'brew-companion-logo-dark.svg',
+    'brew-companion-logo-light.svg',
+    'hero.webp',
+    'uk.png',
+    'israel.png',
+  ],
+  manifest: {
+    name: 'Brew Companion',
+    short_name: 'BrewCompanion',
+    description:
+      'Track your brews, analyze coffee trends, and discover your favorite roasteries',
+    icons: [
+      {
+        src: 'icons/manifest-icon-192.maskable.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: 'icons/manifest-icon-192.maskable.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: 'icons/manifest-icon-512.maskable.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: 'icons/manifest-icon-512.maskable.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+    ],
+    theme_color: '#ba8460',
+    background_color: '#ffffff',
+    display: 'standalone',
+    start_url: '/',
+  },
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugin)],
   server: {
     proxy: {
       '/api': {
