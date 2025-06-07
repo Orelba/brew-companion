4e7907556970ab190f902086353e630d2b1d3a36
Orelba <88292816+Orelba@users.noreply.github.com>
2025-06-07 18:25:43 +0300

    InventoryCardContainer: use the Card component instead of Paper to get a light background on dark mode and some other styles by default

--- client/src/components/InventoryCardContainer/InventoryCardContainer.jsx
+++ client/src/components/InventoryCardContainer/InventoryCardContainer.jsx
@@ -1,17 +1,15 @@
-import { Paper } from '@mantine/core'
+import { Card } from '@mantine/core'
 
 const InventoryCardContainer = ({ children }) => {
   return (
-    <Paper
+    <Card
       shadow='md'
       p='lg'
-      pos='relative'
-      display='flex'
       style={{ flexDirection: 'column', gap: '0.4rem' }}
       bd='1px solid light-dark(var(--mantine-color-gray-1), var(--mantine-color-dark-5))'
     >
       {children}
-    </Paper>
+    </Card>
   )
 }
 
