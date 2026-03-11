a5d1779f4f33a1104a0722af243d2f1ebd3c5019
Orelba <88292816+Orelba@users.noreply.github.com>
2026-03-11 16:57:09 +0200

    fix: remove redundant HTML escaping from notes and grindSetting fields

--- server/middleware/validateBrewData.js
+++ server/middleware/validateBrewData.js
@@ -13,8 +13,7 @@ const validateBrewData = [
   body('brewingMethod', 'Brewing method must be specified.').trim().isMongoId(),
   body('grindSetting', 'Grind setting must be specified.')
     .trim()
-    .isLength({ min: 1 })
-    .escape(),
+    .isLength({ min: 1 }),
   body('time', 'Time must be in the format of MM:SS or HH:MM:SS')
     .optional({ checkFalsy: true })
     .matches(/^([01]\d|2[0-3]):([0-5]\d)(:[0-5]\d)?$/),
@@ -37,7 +36,7 @@ const validateBrewData = [
     .customSanitizer((value) => Math.round(value))
     .withMessage('Temperature must be a number'),
   body('rating', 'Rating must be between 0 to 5').isInt({ min: 0, max: 5 }),
-  body('notes').optional({ values: 'falsy' }).trim().escape(),
+  body('notes').optional({ values: 'falsy' }).trim(),
 ]
 
 export default validateBrewData
